539e803bcd2b609aecdb6dae641b7558ff5aedec
Yutaro Kai <kaikai5.15kaikai@gmail.com>
2019-08-06 01:21:54 +0900

    a

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def handle_message(event):
 
 	line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=choice)
+        TextSendMessage(text="choice")
      )
 
 if __name__ == "__main__":
