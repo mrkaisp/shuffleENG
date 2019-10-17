ae76b293db64fc5b1ce69e363b274836ed5061a5
Yutaro Kai <kaikai5.15kaikai@gmail.com>
2019-10-17 16:18:22 +0900

    a

--- main.py
+++ main.py
@@ -37,13 +37,13 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
+    print("こここ")
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-        
+
     return 'OK'
 
 # MessageEvent
